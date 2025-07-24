1e93b48dd825551b822dba132148c50afa9e1505
Muzzammil <muzzammilneo@gmail.com>
2025-07-24 12:07:16 +0530

    all-working

--- routes/licenseRoutes.js
+++ routes/licenseRoutes.js
@@ -3,8 +3,5 @@ const router = express.Router();
 const { validateLicense } = require("../controllers/licenseController");
 
 router.post("/validate", validateLicense);
-router.get("/", ()=> {
-    return "Hello";
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,11 +9,15 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/license", licenseRoutes);
 
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(process.env.PORT || 3000, () => {
-      console.log("Server running");
+      console.log(`Server running in port: ${process.env.PORT || 3000}`);
     });
   })
   .catch((err) => console.error("MongoDB error:", err));
