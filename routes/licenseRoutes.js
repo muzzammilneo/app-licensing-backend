ab7b5e7f6f0f0e8926815b883187e19f5d4efa56
Muzzammil <muzzammilneo@gmail.com>
2025-07-24 10:26:36 +0530

    root route adding

--- routes/licenseRoutes.js
+++ routes/licenseRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const { validateLicense } = require("../controllers/licenseController");
 
 router.post("/validate", validateLicense);
+router.post("/", (req, res)=> {
+    return "Hello";
+});
 
 module.exports = router;
