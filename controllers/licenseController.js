a63ec45574e9bcd9c6b6b33a7b583ab0853cce4a
Muzzammil <muzzammilneo@gmail.com>
2025-07-25 16:27:25 +0530

    authentication feature for license list

--- controllers/licenseController.js
+++ controllers/licenseController.js
@@ -1,6 +1,8 @@
 const License = require("../models/License");
 
 exports.allLicense = async (req, res) => {
+  if(!req.body) return res.status(400).json({ success: false, message: "You are not authorised to access!" });
+
   const { id } = req.body;
 
   if(id === 'admin') {
@@ -21,7 +23,7 @@ exports.validateLicense = async (req, res) => {
 
   const license = await License.findOne({ licenseKey });
 
-  if (!license) return res.status(403).json({ valid: false, reason: "License not found" });
+  if (!license) return res.status(404).json({ valid: false, reason: "License not found" });
   if (!license.isActive) return res.status(403).json({ valid: false, reason: "License deactivated" });
   if (license.expiresAt && new Date() > license.expiresAt) return res.status(403).json({ valid: false, reason: "License expired" });
 
